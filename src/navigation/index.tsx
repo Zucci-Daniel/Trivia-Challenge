8e3a2aa56b8801428850622c119abd2ce06b0a64
Zucci Daniel <zuccizucci555@gmail.com>
2023-02-01 20:53:26 +0100

    added a smooth navigation animation

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { CardStyleInterpolators, createStackNavigator } from '@react-navigation/stack';
 import { routes } from '../constants/routes';
 import { QuestionScreen, SummaryScreen, WelcomeScreen } from '../screens';
 import navigationService from '../utilities/navigationService';
 
 const Stack = createStackNavigator();
+
+const animation = { cardStyleInterpolator: CardStyleInterpolators.forModalPresentationIOS }
 
 const AppNavigator = () => {
   return (
@@ -19,9 +21,9 @@ const AppNavigator = () => {
         }}
         initialRouteName={routes.welcomeScreen}>
 
-        <Stack.Screen name={routes.welcomeScreen} component={WelcomeScreen} />
-        <Stack.Screen name={routes.questionScreen} component={QuestionScreen} />
-        <Stack.Screen name={routes.summaryScreen} component={SummaryScreen} />
+        <Stack.Screen name={routes.welcomeScreen} component={WelcomeScreen} options={animation} />
+        <Stack.Screen name={routes.questionScreen} component={QuestionScreen} options={animation} />
+        <Stack.Screen name={routes.summaryScreen} component={SummaryScreen} options={animation} />
       </Stack.Navigator>
     </NavigationContainer>
   );
