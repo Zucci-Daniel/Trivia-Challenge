b6af0c10922863daf8fb8b07bb3517ac0c573ba3
Zucci Daniel <zuccizucci555@gmail.com>
2023-02-02 10:34:15 +0100

    worked on close button fix

--- src/components/app-close-button/styles.ts
+++ src/components/app-close-button/styles.ts
@@ -8,7 +8,7 @@ const distance = hp(21);
 export const CloseButtonStyles = StyleSheet.create<CloseButtonStylesProps>({
   container: {
     position: 'absolute',
-    right: distance,
+    right: distance / 2,
     top: distance,
     zIndex: distance,
   },
